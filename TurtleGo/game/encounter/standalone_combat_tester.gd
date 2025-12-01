88835d6d880933762b667bf02531322ff50e4c6c
Josep Valls <josep@valls.name>
2025-12-01 09:45:55 -0800

    tester with buttons

--- TurtleGo/game/encounter/standalone_combat_tester.gd
+++ TurtleGo/game/encounter/standalone_combat_tester.gd
@@ -1,10 +1,21 @@
 extends Node2D
 @export var creature: CreatureData
-
+@onready var combat = $Combat
 func _ready() -> void:
+	var buttons = $HBoxContainer.get_children()
+	for idx in len(buttons):
+		buttons[idx].connect("pressed", test.bind(idx))
+	
+	
+func test(idx):
 	if creature:
-		await $Combat.fight(creature)
-		if $Combat.player_dead:
+		creature.combat_style=idx
+		if combat:
+			combat.queue_free()
+		combat = preload("res://game/encounter/combat.tscn").instantiate()
+		add_child(combat)
+		await combat.fight(creature)
+		if combat.player_dead:
 			prints("nice try mate")
 		else:
 			prints("you win")
